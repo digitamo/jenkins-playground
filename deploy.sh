e2536a357a81eef29e82b15f6b44950b3cb11402
digitamo <osama.khaled.saad@gmail.com>
2020-01-26 00:39:38 +0200

    chore: add Build and deploy steps to jenkins file

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: Change mock deployment to actual deployment on a free server.
+SUCCESS=$((( RANDOM % 2 )))
+
+if [ "$SUCCESS" -eq "1" ]; then
+  echo "SUCCESS";
+  exit 0
+else
+  echo "FAIL";
+  exit 1
+fi
